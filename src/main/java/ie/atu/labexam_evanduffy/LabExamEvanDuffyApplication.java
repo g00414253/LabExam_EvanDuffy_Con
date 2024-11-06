c9d536907cdc851484183441fc7907d2478adc62
G00414253@atu.ie <g00414253@atu.ie>
2024-11-06 14:02:14 +0000

    Created Project and et up repository

---
+++ src/main/java/ie/atu/labexam_evanduffy/LabExamEvanDuffyApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.labexam_evanduffy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabExamEvanDuffyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabExamEvanDuffyApplication.class, args);
+    }
+
+}
