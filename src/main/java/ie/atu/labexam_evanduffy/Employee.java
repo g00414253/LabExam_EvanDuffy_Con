315915e60e483611e5605f79ad8456a2af6cf84f
G00414253@atu.ie <g00414253@atu.ie>
2024-11-06 15:33:17 +0000

    Improved validation

--- src/main/java/ie/atu/labexam_evanduffy/Employee.java
+++ src/main/java/ie/atu/labexam_evanduffy/Employee.java
@@ -1,31 +1,33 @@
 package ie.atu.labexam_evanduffy;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Employee {
 
-    @Pattern(regexp = "EMP-",message = "Invalid Format")
+    @Pattern(regexp = "EMP-.+$",message = "Invalid Format")
     private String employeeCode;
 
-    @Max(value = 100,message = "Maximum Characters")
-    @NotBlank(message = "Must be filled")
+    @Size(max = 100,message = "Maximum Characters")@NotBlank(message = "Must be filled")
     private String name;
 
     @Email(message = "Invalid Email")
     private String email;
 
-    @Pattern(regexp = "Manager",message = "Must be a valid position")
-    @NotBlank(message = "Must be filled")
+    @Pattern(regexp = "Manager",message = "Must be a valid position") @NotBlank(message = "Must be filled")
     private String position;
 
     @Min(value = 1,message = "Must be a positive value")
     private double salary;
 
-    private String dateOfJoining;
+    @Past(message = "Must be a date from today or earlier")
+    private Date dateOfJoining;
 }
